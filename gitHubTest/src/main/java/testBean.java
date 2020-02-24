be7f7ec2ddd2a66787f8f17f811744fd39f0c4b0
禚林昌 <kirinzhuo@163.com>
2020-02-24 17:15:52 +0800

    test commit master1.0.1

--- gitHubTest/src/main/java/testBean.java
+++ gitHubTest/src/main/java/testBean.java
@@ -2,5 +2,7 @@ public class testBean {
     public static void main(String[] args) {
         System.out.println("1111");
         System.out.println("222");
+        System.out.println("333");
+
     }
 }
